8bb4f14a2a348ed3e504fbf59a6e9eae4311c5c3
ranilakshmiammu2 <78391186+ranilakshmiammu2@users.noreply.github.com>
2021-02-08 12:59:15 -0600

    Update viewticket.java
    Updating IP for Sprint 4

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.17.24.226:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.220.110.195:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
