3af2f942dbb61e296172a672130e2261fb428514
ranilakshmiammu2 <78391186+ranilakshmiammu2@users.noreply.github.com>
2021-02-12 06:37:24 -0600

    Update viewticket.java
    updating IP for assignment 3

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.220.110.195:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://3.141.169.227:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
